89239c0ff334d2c29a948794be8b224e12cacfa2
Kymo1970 <kymo1970@gmail.com>
2022-09-20 19:54:07 -0700

    Add javascript to code.

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(event)
+	{
+		let myHeader = document.getElementById("header-id")
+		setTimeOut(() => {
+			myHeader.classList.remove("hidden");
+		}, 3000)
+	});
